bad68e7278212123fc5c094b14518e3d5c4d3cb3
= <daniel20025febrero29@gmail.com>
2024-05-09 12:44:23 -0500

    Function for detect faces

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,9 +7,77 @@ import base64
 from io import BytesIO
 from PIL import Image
 import matplotlib.pyplot as plt
+import os
 
 
-def process_video(image):
+def process_mdf_video(image):
+    # Functions
+    image_data = image
+
+    def detect_bounding_box(vid):
+        cwd = os.getcwd()
+        directory = os.path.join(
+            cwd, 'models', 'haarcascade_frontalface_default.xml')
+        face_classifier = cv2.CascadeClassifier(
+            '/haarcascade_frontalface_default.xml')
+        I_gris = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(I_gris, 1.1, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+        return faces
+
+    def draw(mask, color, textColor, frame):
+        contornos, _ = cv2.findContours(
+            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for c in contornos:
+            area = cv2.contourArea(c)
+            if area > 2000:
+                M = cv2.moments(c)
+                if (M["m00"] == 0):
+                    M["m00"] = 1
+                x = int(M["m10"]/M["m00"])
+                y = int(M["m01"]/M["m00"])
+                cv2.circle(frame, (x, y), 7, (0, 255, 0), -1)
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frame, textColor, (x+10, y), font,
+                            0.75, (0, 255, 0), 1, cv2.LINE_AA)
+
+                nuevoContorno = cv2.convexHull(c)
+                cv2.drawContours(frame, [nuevoContorno], 0, color, 3)
+
+    if image is not None and 'data:image/jpeg;base64,' in image:
+        image_base64 = image.replace('data:image/jpeg;base64,', '')
+        decode_image = base64.b64decode(image_base64)
+        # Decodificar la imagen usando OpenCV
+        im_arr = np.frombuffer(decode_image, dtype=np.uint8)
+        Img = cv2.imdecode(im_arr, cv2.IMREAD_UNCHANGED)
+
+        if Img.shape[0] > 0 and Img.shape[1] > 0 and Img is not None:
+            # plt.imshow(Img)
+            # plt.axis("off")
+            # plt.show()
+
+            # Dibujamos los contornos
+            detect_bounding_box(Img)
+
+            buffer = BytesIO()
+
+            # Mostrar la imagen con los contornos a color original
+            imageColor = cv2.cvtColor(Img, cv2.COLOR_BGR2RGB)
+
+            # Convertir la imagen a BytesIO
+            Image.fromarray(imageColor).save(buffer, format='JPEG')
+
+            # Obtener los datos de la imagen
+            image_data = buffer.getvalue()
+
+    return image_data
+
+
+def process_mdc_video(image):
     # Functions
     image_data = image
 
@@ -43,6 +111,7 @@ def process_video(image):
         # Img = cv2.imdecode(im_arr, -1)
 
         # Colores HSV
+
         # Rojo
         redBajo1 = np.array([0, 100, 20], np.uint8)
         redAlto1 = np.array([5, 255, 255], np.uint8)
@@ -154,11 +223,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             text_data_json = json.loads(text_data)
             message = text_data_json["message"]
             image_data = text_data_json["image_data"]
+            type_model = text_data_json["type_model"]
 
             # Send message to room group
             await self.channel_layer.group_send(
                 self.room_group_name, {"type": "chat.message",
-                                       "message": message, "image_data": image_data}
+                                       "message": message, "image_data": image_data, "type_model": type_model}
             )
         except:
             print('Error inesperado')
@@ -167,10 +237,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event["message"]
         image_data = event["image_data"]
+        type_model = event["type_model"]
 
         try:
+            if type_model == 'mdc':
+                # Model detec color
+                processed_image = process_mdc_video(image_data)
+
+            if type_model == 'mdf':
+                # Model cnn
+                processed_image = process_mdf_video(image_data)
+
             # Procesar la imagen recibida
-            processed_image = process_video(image_data)
 
             # Convertir a base64 de nuevo
             processed_image_base64 = base64.b64encode(
